e2762658f137d8aad86140370308f0c1eaab2e15
Pinki Parmar <parmarpinki932@gmail.com>
2023-05-17 18:55:23 +0530

    Done my work

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -42,7 +42,6 @@ export default function Weather(){
         </form>
         {
             (weather?.name) ?
-            
             <div>
                 <DisplayWeather data={weather} />
             </div>
